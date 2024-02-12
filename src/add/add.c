12503d1a6a6463cf1d8b932a6a4dbad29ee73f21
David Lima <davidlimasstor@gmail.com>
2024-02-12 12:53:59 +0100

    Working compiling and linking a shared library

--- src/add/add.c
+++ src/add/add.c
@@ -1,9 +1,12 @@
 /* add.c
 
-   Demonstrates creating a DLL with an exported function, the inflexible way.
+   Demonstrates creating a DLL with an exported function in a flexible and
+   elegant way.
 */
 
-__declspec(dllexport) int __cdecl Add(int a, int b)
+#include "add.h"
+
+int ADDCALL Add(int a, int b)
 {
   return (a + b);
 }
